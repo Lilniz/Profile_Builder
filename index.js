299c76562b49d751facf7a00b0983e9d7b13131c
Lizzle <leefboettcher@gmail.com>
2022-08-31 00:45:04 -0500

    rearranged file names for continuity, as well as added in push for dist folder html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const protoHTML = require('./src/protoHTML.js');
+const generateHTML = require('./src/generateHTML');
 
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
